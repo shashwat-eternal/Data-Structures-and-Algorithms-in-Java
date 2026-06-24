1a440dd0cc1401d2cdb3ed4b2b51d695a3a40ed4
Shashwat Shukla <shashwatshukklla@gmail.com>
2026-06-24 10:48:47 +0530

    Time: 3 ms (22.8%), Space: 43 MB (11.39%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    private int count = 0;
+
+    public int totalNQueens(int n) {
+        backtrack(0, n, new HashSet<>(), new HashSet<>(), new HashSet<>());
+        return count;
+    }
+
+    private void backtrack(int row, int n,
+                           Set<Integer> cols, Set<Integer> diag1, Set<Integer> diag2) {
+        if (row == n) {
+            count++;
+            return;
+        }
+        for (int col = 0; col < n; col++) {
+            if (cols.contains(col) || diag1.contains(row - col) || diag2.contains(row + col)) continue;
+            cols.add(col);
+            diag1.add(row - col);
+            diag2.add(row + col);
+
+            backtrack(row + 1, n, cols, diag1, diag2);
+
+            cols.remove(col);
+            diag1.remove(row - col);
+            diag2.remove(row + col);
+        }
+    }
+}
