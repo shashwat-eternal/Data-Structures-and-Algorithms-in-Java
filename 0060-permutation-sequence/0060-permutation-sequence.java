1fb7bc24a0659eef238e55225a53432a98e47ae4
Shashwat Shukla <shashwatshukklla@gmail.com>
2026-06-26 11:56:10 +0530

    Time: 1 ms (99.61%), Space: 43 MB (25.96%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public String getPermutation(int n, int k) {
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 1; i <= n; i++) nums.add(i);
+        int[] fact = new int[n + 1];
+        fact[0] = 1;
+        for (int i = 1; i <= n; i++) fact[i] = fact[i - 1] * i;
+        k--;
+        StringBuilder sb = new StringBuilder();
+        for (int i = n; i >= 1; i--) {
+            int idx = k / fact[i - 1];
+            sb.append(nums.get(idx));
+            nums.remove(idx);
+            k %= fact[i - 1];
+        }
+        return sb.toString();
+    }
+}
