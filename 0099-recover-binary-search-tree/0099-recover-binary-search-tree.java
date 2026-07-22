c9572785974ac225379beae3cd949296515df13c
Shashwat Shukla <shashwatshukklla@gmail.com>
2026-07-22 22:09:03 +0530

    Time: 1 ms (100%), Space: 46.6 MB (49.67%) - LeetHub

---
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -0,0 +1,29 @@
+class Solution {
+    private TreeNode first = null;
+    private TreeNode second = null;
+    private TreeNode prev = new TreeNode(Integer.MIN_VALUE);
+
+    public void recoverTree(TreeNode root) {
+        inorder(root);
+        
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    private void inorder(TreeNode node) {
+        if (node == null) return;
+
+        inorder(node.left);
+
+        if (first == null && prev.val > node.val) {
+            first = prev;
+        }
+        if (first != null && prev.val > node.val) {
+            second = node;
+        }
+        prev = node;
+
+        inorder(node.right);
+    }
+}
