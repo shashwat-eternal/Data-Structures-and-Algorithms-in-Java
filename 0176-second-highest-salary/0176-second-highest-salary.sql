f27ebe0ca18332ba57425719bb94d7433ea8dc3c
Shashwat Shukla <shashwatshukklla@gmail.com>
2026-07-30 09:12:48 +0530

    Time: 284 ms (59.62%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,8 @@
+SELECT 
+    IFNULL(
+        (SELECT DISTINCT salary 
+         FROM Employee 
+         ORDER BY salary DESC 
+         LIMIT 1 OFFSET 1),
+        NULL
+    ) AS SecondHighestSalary;
