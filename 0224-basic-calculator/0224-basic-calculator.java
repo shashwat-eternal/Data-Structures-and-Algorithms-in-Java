6ff2f4c6ee511239ad8e6f52a5b0ef7b3fe3c03e
Shashwat Shukla <shashwatshukklla@gmail.com>
2026-08-02 13:18:26 +0530

    Time: 10 ms (80.07%), Space: 46.2 MB (93.51%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int calculate(String s) {
+        int result = 0, sign = 1, num = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
+            } else if (c == '+') {
+                result += sign * num;
+                num = 0;
+                sign = 1;
+            } else if (c == '-') {
+                result += sign * num;
+                num = 0;
+                sign = -1;
+            } else if (c == '(') {
+                stack.push(result);
+                stack.push(sign);
+                result = 0;
+                sign = 1;
+            } else if (c == ')') {
+                result += sign * num;
+                num = 0;
+                result *= stack.pop(); 
+                result += stack.pop(); 
+            }
+        }
+        return result + sign * num;
+    }
+}
