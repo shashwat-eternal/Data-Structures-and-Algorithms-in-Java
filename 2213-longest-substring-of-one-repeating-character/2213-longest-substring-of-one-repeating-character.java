ecbec346edf0dda5a541cb7138d0b9f4e3ae75bc
Shashwat Shukla <shashwatshukklla@gmail.com>
2026-08-13 07:03:01 +0530

    Time: 111 ms (43.75%), Space: 157.8 MB (10.94%) - LeetHub

---
+++ 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.java
@@ -0,0 +1,110 @@
+class Solution {
+    static class Node {
+        int leftLen;
+        int rightLen;
+        int maxLen;
+        int len;
+        char leftChar;
+        char rightChar;
+
+        Node() {}
+
+        Node(char c) {
+            leftChar = rightChar = c;
+            leftLen = rightLen = maxLen = len = 1;
+        }
+    }
+
+    Node[] tree;
+    char[] arr;
+
+    public int[] longestRepeating(String s, String queryCharacters, int[] queryIndices) {
+        int n = s.length();
+        int k = queryIndices.length;
+
+        arr = s.toCharArray();
+        tree = new Node[4 * n];
+
+        build(1, 0, n - 1);
+
+        int[] ans = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            int index = queryIndices[i];
+            char ch = queryCharacters.charAt(i);
+
+            arr[index] = ch;
+            update(1, 0, n - 1, index, ch);
+
+            ans[i] = tree[1].maxLen;
+        }
+
+        return ans;
+    }
+
+    private void build(int node, int l, int r) {
+        if (l == r) {
+            tree[node] = new Node(arr[l]);
+            return;
+        }
+
+        int mid = l + (r - l) / 2;
+
+        build(node * 2, l, mid);
+        build(node * 2 + 1, mid + 1, r);
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    private void update(int node, int l, int r, int index, char ch) {
+        if (l == r) {
+            tree[node] = new Node(ch);
+            return;
+        }
+
+        int mid = l + (r - l) / 2;
+
+        if (index <= mid) {
+            update(node * 2, l, mid, index, ch);
+        } else {
+            update(node * 2 + 1, mid + 1, r, index, ch);
+        }
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    private Node merge(Node a, Node b) {
+        Node res = new Node();
+
+        res.len = a.len + b.len;
+
+        res.leftChar = a.leftChar;
+        res.rightChar = b.rightChar;
+
+       
+        res.leftLen = a.leftLen;
+
+        if (a.leftLen == a.len && a.rightChar == b.leftChar) {
+            res.leftLen = a.len + b.leftLen;
+        }
+
+
+        res.rightLen = b.rightLen;
+
+        if (b.rightLen == b.len && a.rightChar == b.leftChar) {
+            res.rightLen = b.len + a.rightLen;
+        }
+
+        res.maxLen = Math.max(a.maxLen, b.maxLen);
+
+       
+        if (a.rightChar == b.leftChar) {
+            res.maxLen = Math.max(
+                res.maxLen,
+                a.rightLen + b.leftLen
+            );
+        }
+
+        return res;
+    }
+}
